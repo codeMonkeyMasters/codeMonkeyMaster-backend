724e57f23226e1ea93085ea86dd7e4d505057881
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-24 19:18:03 +0200

    Router setup for fastest times within specific exercise

--- routers/exercisesRouter.js
+++ routers/exercisesRouter.js
@@ -6,26 +6,71 @@ const CompletedExercises = require("../models").completedExercise
 
 const router = new Router()
 
+router.get(
+    "/:id/fastest",
+    async(req, res, next) => {
+        const exerciseIdNeeded = parseInt(req.params.id)
+        // console.log("exercise id test", exerciseIdNeeded)
+        if(!exerciseIdNeeded){
+            res.status(400).send("Url malfunctioned, please refresh and try again.")
+        }
+        try{
+            const exercisesCompleted = await CompletedExercises.findAll({
+                where: {
+                    exerciseId: exerciseIdNeeded,
+                }
+            })
+            // console.log("completed exercises test", exercisesCompleted)
+            if(!exercisesCompleted){
+                res.status(404).send("Oops it seems we couldnt find the completed exercises, refresh and try again.")
+            }
+
+            const sortedExercisesCompleted = exercisesCompleted.sort((a, b) => {
+                const timeTakenA = a.timeTaken
+                // console.log(timeTakenA)
+                const timeTakenB = b.timeTaken
+                // console.log(timeTakenB)
+                if(timeTakenB > timeTakenA){
+                    return -1
+                } else if(timeTakenA > timeTakenB){
+                    return 1
+                } else {
+                    return 0
+                }
+            })
+            // console.log("sorted exercises completed", sortedExercisesCompleted)
+            if(!sortedExercisesCompleted){
+                res.status(400).send("Malfunction in the backend, refresh and try again.")
+            } else {
+                res.status(202).send(sortedExercisesCompleted)
+            }
+
+        } catch(error){
+            next(error)
+        }
+    }
+)
+
 router.patch(
     "/:id/completed",
     authMiddleware,
     async(req, res, next) => {
         const userIdNeeded = req.user.id
-        console.log("user id test:", userIdNeeded)
+        // console.log("user id test:", userIdNeeded)
         if(!userIdNeeded){
             res.status(401).send("Sorry you're not suppose to be here, please login in or sign up to continue.")
         }
 
         const exerciseIdNeeded = parseInt(req.params.id)
-        console.log("exercise Id test", exerciseIdNeeded)
+        // console.log("exercise Id test", exerciseIdNeeded)
         if(!exerciseIdNeeded){
             res.status(400).send("The Url malfunctioned please refresh and try again.")
         }
 
         const time = req.body.timeTaken
         const expInt = parseInt(req.body.exp)
-        console.log(`time test: ${time}
-        exp test: ${expInt}`)
+        // console.log(`time test: ${time}
+        // exp test: ${expInt}`)
         if(!time || !expInt){
             res.status(400).send("Please provide the proper credentials to continue.")
         }
