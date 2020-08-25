f907ed9a432c05e7936a5c619fb7997c8ffe10e0
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-25 12:11:08 +0200

    Adding proper questions to seeders

--- seeders/3-some-quizQuestions.js
+++ seeders/3-some-quizQuestions.js
@@ -5,15 +5,25 @@ module.exports = {
     await queryInterface.bulkInsert(
       'quizQuestions', [
       {
-        question: "What is my name?",
-        answer: "darian",
-        incorrect1: "Susuan",
-        incorrect2: "Brad",
-        incorrect3: "Brent",
+        question: "What does the Map() accomplish?",
+        answer: "Creates a new array populated with the results of calling a provided function on every element in the calling array.",
+        incorrect1: "Calls a provided callback function once for each element in the array, that transforms the old array.",
+        incorrect2: "It calls for all elements of the array including the missing elements and creates a new array with the results.",
+        incorrect3: "Populates the original array with the results of calling a provided function on every element in the calling array.",
         exerciseId: 1,
         createdAt: new Date(),
         updatedAt: new Date(),
-      }
+      },
+      {
+        question: "Which of these answers uses the correct syntax for the Map()",
+        answer: "array.map( x => x * a)",
+        incorrect1: "array.map(x * a)",
+        incorrect2: "array.map(() = x => x * a)",
+        incorrect3: "array.map(x = x * a)",
+        exerciseId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ]
     )},
 
