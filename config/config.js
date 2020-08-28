2225f05175b22b623ce3bc8cfcdc7c11dea07123
DiegoOTdC <studioartdiego@gmail.com>
2020-08-28 13:23:19 +0200

    change production config.js

--- config/config.js
+++ config/config.js
@@ -1,25 +1,22 @@
-require("dotenv").config()
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "url": process.env.DATABASE_URL_DEV,
-    "dialect": "postgres",
-    "operatorsAliases": "0"
+  development: {
+    url: process.env.DATABASE_URL_DEV,
+    dialect: "postgres",
+    operatorsAliases: "0",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "operatorsAliases": "0"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: "0",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "operatorsAliases": "0"
-  }
-}
+  production: {
+    url: process.env.DATABASE_URL_DEV,
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+};
